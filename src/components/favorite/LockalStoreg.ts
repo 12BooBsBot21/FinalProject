56d59b2b8ca091a112eb87de818158608f74e70d
Tsehan <yourmortyhan52@gmail.com>
2026-04-07 10:04:52 +0300

    функция loudLockSrot доделана, работа с lockalStore окончена

--- src/components/favorite/LockalStoreg.ts
+++ src/components/favorite/LockalStoreg.ts
@@ -1,17 +1,25 @@
 import type { FavoriteCharacter } from "../../types";
-
-export function loadLockStor(): string {
-  return JSON.stringify([]);
+const keyLS: string = "favorites";
+export function loadLockStor(): FavoriteCharacter[] {
+  try {
+    const dataLS = localStorage.getItem(keyLS);
+    if (!dataLS) return [];
+    const parseDataLS: FavoriteCharacter[] = JSON.parse(dataLS);
+    if (!Array.isArray(parseDataLS)) return [];
+    return parseDataLS;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 export function saveLockStor(character: FavoriteCharacter): void {
   if (!character) return;
   try {
-    const keyLS: string = "favorites";
-    const loadFavorites = JSON.parse(loadLockStor());
+    const loadFavorites = loadLockStor();
     loadFavorites.push(character);
     const allCharacter = loadFavorites;
     localStorage.setItem(keyLS, JSON.stringify(allCharacter));
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 }
