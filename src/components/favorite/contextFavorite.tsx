0115337c1b44cc878827d096e6e1b8feaba887e2
Tsehan <yourmortyhan52@gmail.com>
2026-04-07 12:53:22 +0300

    начал создавать интеграцию контекста и контекст провайдер

---
+++ src/components/favorite/contextFavorite.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from "react";
+import type { FavoriteCharacter } from "../../types";
+
+interface FavoritesContextType {
+  favorite: FavoriteCharacter[];
+  isFavorite: (id: number) => boolean;
+  toggleFavorite: (character: FavoriteCharacter) => void;
+}
+const favoriteContext = createContext<FavoritesContextType | undefined>(
+  undefined,
+);
+export function ContextFavoriteProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [favorite, setFavorite] = useState<FavoriteCharacter[]>([]);
+  function isFavorite(id: number) {
+    if (typeof id === "number") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function toggleFavorite(character: FavoriteCharacter) {
+    const result = favorite;
+    result.push(character);
+    setFavorite(result);
+  }
+  <favoriteContext.Provider value={{ favorite, isFavorite, toggleFavorite }}>
+    {children}
+  </favoriteContext.Provider>;
+}
